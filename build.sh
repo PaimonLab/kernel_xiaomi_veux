a6522141af7682d0ec74d0b9023dbfbf6bdce06e
Amrita Das <bhabanidas431@gmail.com>
2026-05-04 06:55:26 +0530

    build.sh: Fixup Anykernel3 Errors
    Change-Id: Ie91a3e81c54ba4a99570cac0496ab82341f4df31

--- build.sh
+++ build.sh
@@ -218,13 +218,15 @@ function zipping() {
     cd ${AnyKernelPath} || exit 1
     if [ "$KERNELSU" = "yes" ];then
       VARIANT="[KSU] "
-      sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME}) | KernelSU Version: ${KERNELSU_VERSION}/g" anykernel.sh
+      sed -i "s|kernel.string=.*|kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME}) | KernelSU Version: ${KERNELSU_VERSION}|g" anykernel.sh
     else
       VARIANT="[Non-KSU] "
-      sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})/g" anykernel.sh
+      sed -i "s|kernel.string=.*|kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})|g" anykernel.sh
     fi
     zip -r9 "${VARIANT}${KERNEL_ZIP}" * -x .git README.md *placeholder
     mv "${VARIANT}${KERNEL_ZIP}" ~/
+    ZIP_PATH="$(pwd)/${VARIANT}${KERNEL_ZIP}"
+    echo "ZIP_PATH=$ZIP_PATH" >> $GITHUB_ENV
     cd ..
     sudo rm -rf ${AnyKernelPath}
     cleanup
