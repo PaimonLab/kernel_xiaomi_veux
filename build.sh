b3d565da43942bd1c1067843218ab01253b65a31
Amrita Das <bhabanidas431@gmail.com>
2026-05-04 20:24:22 +0530

    build.sh: fix zip naming scheme
    Change-Id: Icab3af53fbf5470937faf390a8ccbfdbb7bd363a

--- build.sh
+++ build.sh
@@ -223,9 +223,9 @@ function zipping() {
       VARIANT="[Non-KSU] "
       sed -i "s|kernel.string=.*|kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})|g" anykernel.sh
     fi
-    zip -r9 "${VARIANT}${KERNEL_ZIP}" * -x .git README.md *placeholder
-    mv "${VARIANT}${KERNEL_ZIP}" ~/
-    ZIP_PATH="$(pwd)/${VARIANT}${KERNEL_ZIP}"
+    zip -r9 "${VARIANT}-${KERNEL_ZIP}" * -x .git README.md *placeholder
+    mv "${VARIANT}-${KERNEL_ZIP}" ~/
+    ZIP_PATH="$(pwd)/${VARIANT}-${KERNEL_ZIP}"
     echo "ZIP_PATH=$ZIP_PATH" >> $GITHUB_ENV
     cd ..
     sudo rm -rf ${AnyKernelPath}
